a0ea2514ea69e26935adf4226a9bd4a45c3ed758
EyePulp <eyepulp@eyepulp.net>
2015-03-13 14:39:25 -0500

    simplify mapping

--- example.usage.js
+++ example.usage.js
@@ -13,42 +13,37 @@ var settings = {
         { host: 'es-server-2.example.com',port: 9200}
     ],
 
-    // the way you want your data to be handled (turn off analysis for strings you don't want broken into tokens)
-    //
-    mapping : {
-        templates: {
-            // this usually matches your prefix
-            "foobar": {
-                // this pattern should match your indexes to be created -- use the wildcard, Luke
-                "template" : "foobar-*",
-                "order": 0,
-                "settings": {},
-                "mappings": {
-                    // the actual mappings for different _types of docs
-                    // http://www.elastic.co/guide/en/elasticsearch/reference/current/mapping.html
-                    "hit" : {
-                        "properties":{
-                            "id"              : {"index": "not_analyzed", "type": "string"},
-                            "domain_name"     : {"index": "not_analyzed", "type": "string"},
-                            "browser"         : {"index": "not_analyzed", "type": "string"},
-                            "http_referer"    : {"index": "not_analyzed", "type": "string", "ignore_above": 512},
-                            "http_user_agent" : {"index": "not_analyzed", "type": "string", "ignore_above": 256},
-                            "ip_address"      : {"index": "not_analyzed", "type": "string"},
-                            "server"          : {"index": "not_analyzed", "type": "string"},
-                            "querystring"     : {"index": "not_analyzed", "type": "string", "ignore_above": 256},
-                            "os.family"       : {"index": "not_analyzed", "type": "string"},
-                        }
-                    },
-                    "health": {
-                        "properties": {
-                            "id"     : {"index": "not_analyzed","type": "string"},
-                            "server" : {"index": "not_analyzed","type": "string"}
-                        }
-                    },
-                },
-                "aliases": {}
-            }
-        }
+    // the way you want your data to be handled
+    // (turn off analysis for strings you don't want broken into tokens)
+    mapping: {
+        // this pattern should include your prefix -- use the wildcard, Luke
+        "template" : "foobar-*",
+        "order": 0,
+        "settings": {},
+        "mappings": {
+            // the actual mappings for different _types of docs
+            // http://www.elastic.co/guide/en/elasticsearch/reference/current/mapping.html
+            "hit" : {
+                "properties":{
+                    "id"              : {"index": "not_analyzed", "type": "string"},
+                    "domain_name"     : {"index": "not_analyzed", "type": "string"},
+                    "browser"         : {"index": "not_analyzed", "type": "string"},
+                    "http_referer"    : {"index": "not_analyzed", "type": "string", "ignore_above": 512},
+                    "http_user_agent" : {"index": "not_analyzed", "type": "string", "ignore_above": 256},
+                    "ip_address"      : {"index": "not_analyzed", "type": "string"},
+                    "server"          : {"index": "not_analyzed", "type": "string"},
+                    "querystring"     : {"index": "not_analyzed", "type": "string", "ignore_above": 256},
+                    "os.family"       : {"index": "not_analyzed", "type": "string"},
+                }
+            },
+            "health": {
+                "properties": {
+                    "id"     : {"index": "not_analyzed","type": "string"},
+                    "server" : {"index": "not_analyzed","type": "string"}
+                }
+            },
+        },
+        "aliases": {}
     }
 };
 // set up the logging object
@@ -59,7 +54,7 @@ var esl = require('./lib/eslogger')(settings);
 esl.mapping();
 
 // how to log:
-// log a doc
+// esl.log(_type,doc,[req (optional)]);
 esl.log('hit',{'foo':'bar'});
 
 
